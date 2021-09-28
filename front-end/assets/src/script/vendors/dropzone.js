24f380eeaef1f241eceaf9197c698316ab5c7edd
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2021-09-28 16:54:25 +0300

    Create NFT Finished

--- front-end/assets/src/script/vendors/dropzone.js
+++ front-end/assets/src/script/vendors/dropzone.js
@@ -24,3 +24,33 @@ let createNftUploadFile = new Dropzone(".createnftuploadfile", {
 createNftUploadFile.on("addedfile", file => {
     console.log(`File added: ${file.name}`);
 });
+
+let createNftUploadDoc01 = new Dropzone(".createnftuploaddoc01", {
+    url: "/file/post",
+    paramName: "images", // The name that will be used to transfer the file
+    maxFiles: 1,
+    maxFilesize: 2, // MB
+    autoProcessQueue: false,
+    uploadMultiple: false,
+    acceptedFiles: ".jpeg,.jpg,.png,.gif",
+    thumbnailWidth: null,
+    thumbnailHeight: null,
+    resizeQuality: 1,
+    resizeMethod: "crop",
+    init: function() {}
+});
+
+let createNftUploadDoc02 = new Dropzone(".createnftuploaddoc02", {
+    url: "/file/post",
+    paramName: "images", // The name that will be used to transfer the file
+    maxFiles: 1,
+    maxFilesize: 2, // MB
+    autoProcessQueue: false,
+    uploadMultiple: false,
+    acceptedFiles: ".jpeg,.jpg,.png,.gif",
+    thumbnailWidth: null,
+    thumbnailHeight: null,
+    resizeQuality: 1,
+    resizeMethod: "crop",
+    init: function() {}
+});
