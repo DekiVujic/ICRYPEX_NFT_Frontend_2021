10807c5ba1c404b2551939bf62a48eb196e74463
Berker Parçalı <berker.parcali@icrypex.com>
2021-10-07 12:08:05 +0300

    signup ok

--- react-app/src/screens/SingUp/index.js
+++ react-app/src/screens/SingUp/index.js
@@ -1,19 +1,16 @@
 import {Fragment, useEffect, useState} from "react";
 import {
     Button,
-    Card,
     Col,
-    Container,
     FloatingLabel,
-    Form, FormCheck,
+    Form, 
+    FormCheck,
     FormControl,
-    FormFloating,
     FormGroup, InputGroup,
     Row
 } from "react-bootstrap";
 import PasswordStrengthBar from 'react-password-strength-bar';
 import { useTranslation } from "react-i18next";
-import icrypexNft from "../../assets/img/content/login_form_logo_icrypex.svg";
 import { dictSignIn } from "../../constants";
 import { IconSet } from "../../components/IconSet.jsx";
 import { useSelector } from "react-redux";
@@ -24,7 +21,10 @@ import { registerApi, getCountryCodesApi} from "../../api";
 const SignUp = () => {
     const { t } = useTranslation(["common", "login"]);
     const { lang } = useSelector(state => state.ui);
+    const [passShown, setPassShown] = useState(false);
+    const [repassShown, setRepassShown] = useState(false);
     const [password, setPassword] = useState('');
+    const [phoneArea, setPhoneArea] = useState('90');
     const [countryCodesData, setCountryCodesData] = useState([]);
 
     const {
@@ -44,6 +44,18 @@ const SignUp = () => {
         },
     });
 
+    const changePassShown = () => {
+        setPassShown(passShown ? false : true);
+    };
+
+    const changeRepassShown = () => {
+        setRepassShown(repassShown ? false : true);
+    };
+
+    const handlePhoneAreaChange = e => {
+        setPhoneArea(e.target.value);
+    };
+
     useEffect(() => {
         callCountryCodesApi();
     }, []);
@@ -62,7 +74,7 @@ const SignUp = () => {
             name : values.firstname,
             surname: values.surname,
             email : values.email,
-            phone : values.phonearea + values.phonenumber,
+            phone : phoneArea + values.phonenumber,
             password : password})
             .then(res => {
                 console.log(res);
@@ -84,11 +96,11 @@ const SignUp = () => {
                     <div className="container">
                         <div className="loginarea-box signup">
                             <div className="loginarea-headtitle sitecontent sitecontent-title sitecontent-center">
-                                <h2>Sign Up</h2>
-                                <p>Already have an account? <a href=""><u>Sign In</u></a></p>
+                                <h2>{t("common:signUp")}</h2>
+                                <p>{t("common:alreadyHaveAnAccount")} <a href={dictSignIn[lang]}><u>{t("common:signIn")}</u></a></p>
                             </div>
                             <div className="loginarea-darkinfo sitecontent sitecontent-title sitecontent-center">
-                                <h4>One Account, two platforms</h4>
+                                <h4>{t("common:oneAccountTwoPlatforms")}</h4>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                                 <div className="logoarea textusernotselect">
                                     <div className="logoarea-item"><img src="../assets/img/content/login_form_logo_icrypex_nft.svg"/></div>
@@ -96,50 +108,133 @@ const SignUp = () => {
                                 </div>
                             </div>
                             <div className="loginarea-form">
-                                <Form className="siteform">
+                                <Form className="siteform"
+                                    autoComplete="off"
+                                    noValidate
+                                    onSubmit={handleSubmit(onSubmit)}>
                                     <div className="formfieldset">
                                         <Row className="g-3">
                                             <Col className="col-lg">
-                                                <FloatingLabel className="form-group" label="First Name">
-                                                    <FormControl size="lg" placeholder="..." />
+                                                <FloatingLabel className="form-group" label={t("login:firstname")}>
+                                                    <FormControl size="lg" 
+                                                        type="text"
+                                                        name="firstname"
+                                                        placeholder={t("login:firstname")}
+                                                        aria-invalid={errors.firstname ? "true" : "false"}
+                                                        {...register('firstname', { required: true })} />
+                                                    {errors.firstname && (
+                                                        <Form.Text className="text-muted">
+                                                            {t("form:isRequired")}
+                                                        </Form.Text>
+                                                    )}
                                                 </FloatingLabel>
                                             </Col>
                                             <Col className="col-lg">
-                                                <FloatingLabel className="form-group" label="Last Name">
-                                                    <FormControl size="lg" placeholder="..." />
+                                                <FloatingLabel className="form-group" label={t("login:surname")}>
+                                                    <FormControl size="lg" 
+                                                        type="text"
+                                                        name="surname"
+                                                        placeholder={t("login:surname")}
+                                                        aria-invalid={errors.surname ? "true" : "false"}
+                                                        {...register('surname', { required: true })} />
+                                                    {errors.surname && (
+                                                        <Form.Text className="text-muted">
+                                                            {t("form:isRequired")}
+                                                        </Form.Text>
+                                                    )}
                                                 </FloatingLabel>
                                             </Col>
                                         </Row>
                                         <FloatingLabel className="form-group" label="Username">
-                                            <FormControl size="lg" placeholder="..." />
+                                            <FormControl size="lg" 
+                                                type="text"
+                                                name="username"
+                                                placeholder={t("login:username")}
+                                                aria-invalid={errors.username ? "true" : "false"}
+                                                {...register('username', { required: true })}/>
+                                            {errors.username && (
+                                                <Form.Text className="text-muted">
+                                                    {t("form:isRequired")}
+                                                </Form.Text>
+                                            )}
                                         </FloatingLabel>
                                         <FormGroup className="form-group phonearea">
                                             <InputGroup>
-                                                <FloatingLabel className="input-group-append" label="Phone">
-                                                    <Form.Select className="form-select-lg">
-                                                        <option value="90" selected="">90</option>
-                                                        <option value="357">357</option>
-                                                        <option value="1">1</option>
+                                                <FloatingLabel className="input-group-append" label={t("login:phone")}>
+                                                    <Form.Select 
+                                                        className="form-select-lg"
+                                                        name="phonearea"
+                                                        value={phoneArea}
+                                                        onChange={handlePhoneAreaChange}>
+                                                        {countryCodesData.map((val, index) => {
+                                                            return <option key={val.country_code}>{val.country_code}</option>
+                                                        })}
                                                     </Form.Select>
+                                                    {errors.phonearea && (
+                                                        <Form.Text className="text-muted">
+                                                            {t("form:isRequired")}
+                                                        </Form.Text>
+                                                    )}
                                                 </FloatingLabel>
-                                                <FloatingLabel className="col" label="Phone">
-                                                    <FormControl size="lg" placeholder="..." />
+                                                <FloatingLabel className="col" label={t("login:phone")}>
+                                                    <FormControl size="lg"
+                                                        type="text"
+                                                        name="phonenumber"
+                                                        placeholder={t("login:phone")}
+                                                        aria-invalid={errors.phonenumber ? "true" : "false"}
+                                                        {...register('phonenumber', { required: true })}/>
+                                                    {errors.phonenumber && (
+                                                        <Form.Text className="text-muted">
+                                                            {t("form:isRequired")}
+                                                        </Form.Text>
+                                                    )}
                                                 </FloatingLabel>
                                             </InputGroup>
                                         </FormGroup>
                                         <FloatingLabel className="form-group" label="Email">
-                                            <FormControl type="email" size="lg" placeholder="..." />
+                                            <FormControl size="lg"
+                                                type="email"
+                                                name="email"
+                                                placeholder={t("login:email")}
+                                                aria-invalid={errors.email ? "true" : "false"}
+                                                {...register('email', { required: true })} />
+                                            {errors.email && (
+                                                <Form.Text className="text-muted">
+                                                    {t("form:isRequired")}
+                                                </Form.Text>
+                                            )}
                                         </FloatingLabel>
                                         <FloatingLabel className="form-group showhidepass" label="Password">
-                                            <FormControl type="password" size="lg" placeholder="..." />
-                                            <Button className="showhidebtn">
-                                                <IconSet sprite="btnssmclrd" size="24" name="eyeshow" />
+                                            <FormControl size="lg" 
+                                                type={passShown ? "text" : "password"}
+                                                name="pass"
+                                                placeholder={t("login:password")}
+                                                aria-invalid={errors.pass ? "true" : "false"}
+                                                value={password}
+                                                onChange={e => setPassword(e.target.value)} />                                            
+                                            <Button className="showhidebtn" onClick={changePassShown}>
+                                                <IconSet sprite="btnssmclrd" 
+                                                    size="24" 
+                                                    name={passShown ? "eyehide" : "eyeshow"} />
                                             </Button>
                                         </FloatingLabel>
+                                        <PasswordStrengthBar password={password} />
                                         <FloatingLabel className="form-group showhidepass" label="Confirm Password">
-                                            <FormControl type="password" size="lg" placeholder="..." />
-                                            <Button className="showhidebtn">
-                                                <IconSet sprite="btnssmclrd" size="24" name="eyehide" />
+                                            <FormControl size="lg" 
+                                                type={repassShown ? "text" : "password"}
+                                                name="repass"
+                                                placeholder={t("login:confirmPassword")}
+                                                aria-invalid={errors.repass ? "true" : "false"}
+                                                {...register('repass', { required: true })}/>
+                                            {errors.repass && (
+                                                <Form.Text className="text-muted">
+                                                    {t("form:isRequired")}
+                                                </Form.Text>
+                                            )}
+                                            <Button className="showhidebtn" onClick={changeRepassShown}>
+                                                <IconSet sprite="btnssmclrd" 
+                                                    size="24"
+                                                    name={repassShown ? "eyehide" : "eyeshown"} />
                                             </Button>
                                         </FloatingLabel>
                                         <FormGroup className="form-group checkboxarea">
@@ -151,11 +246,11 @@ const SignUp = () => {
                                                     <label>I'm not a robot</label>
                                                 </div>
                                             </FormCheck>
-                                            <Form.Check type="checkbox" label="By clicking Sign Up, I hereby acknowledge that I agree to the ICRYPEX NFT Terms and Conditions and I’ve read the Privacy Notice." />
-                                            <Form.Check type="checkbox" label="I want to receive news from commercial advertisements and announcements." />
+                                            <Form.Check type="checkbox" label={t("login:readAndAgreeClick")} />
+                                            <Form.Check type="checkbox" label={t("login:readAndAgreeNews")} />
                                         </FormGroup>
                                         <FormGroup>
-                                            <Button className="w-100" size="lg" variant="primary" type="submit">Sign Up</Button>
+                                            <Button className="w-100" size="lg" variant="primary" type="submit">{t("common:signUp")}</Button>
                                         </FormGroup>
                                     </div>
                                 </Form>
@@ -165,7 +260,7 @@ const SignUp = () => {
                 </section>
             </main>
 
-            <Container className="justify-content-center pt-5 d-none" style={{ height: "80rem" }}>
+            {/* <Container className="justify-content-center pt-5 d-none" style={{ height: "80rem" }}>
                 <Row className="justify-content-md-center">
                     <Card style={{ width: '35rem', height: '65rem' }} className="text-center">
                         <Card.Body >
@@ -331,7 +426,6 @@ const SignUp = () => {
                                                         {t("form:isRequired")}
                                                     </Form.Text>
                                                 )}
-                                                {/* <Button className="showhidebtn" type="button">btn</Button> */}
                                             </Form.Group>
                                             <Form.Group className="checkboxarea" controlId="formBasicCheckbox">
                                                 <Form.Check type="checkbox" label={t("login:readAndAgreeClick")} />
@@ -349,7 +443,7 @@ const SignUp = () => {
                         </Card.Body>
                     </Card>
                 </Row>
-            </Container>
+            </Container> */}
 
         </Fragment>
     )
