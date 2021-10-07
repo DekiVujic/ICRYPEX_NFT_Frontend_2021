d16f4c8644126cbd488d58af7db626bcf841aeba
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2021-10-07 18:10:01 +0300

    Detail NFT Finished

--- front-end/assets/src/script/vendors/dropzone.js
+++ front-end/assets/src/script/vendors/dropzone.js
@@ -6,51 +6,56 @@ import Dropzone from "dropzone";
 // This behaviour will change in future versions.
 Dropzone.autoDiscover = false;
 
-let createNftUploadFile = new Dropzone(".createnftuploadfile", {
-    url: "/file/post",
-    paramName: "images", // The name that will be used to transfer the file
-    maxFiles: 1,
-    maxFilesize: 2, // MB
-    autoProcessQueue: false,
-    uploadMultiple: false,
-    acceptedFiles: ".jpeg,.jpg,.png,.gif",
-    thumbnailWidth: null,
-    thumbnailHeight: null,
-    resizeQuality: 1,
-    resizeMethod: "crop",
-    init: function() {}
-});
 
-createNftUploadFile.on("addedfile", file => {
-    console.log(`File added: ${file.name}`);
-});
+if(document.querySelector('.createnftarea-form')){
 
-let createNftUploadDoc01 = new Dropzone(".createnftuploaddoc01", {
-    url: "/file/post",
-    paramName: "images", // The name that will be used to transfer the file
-    maxFiles: 1,
-    maxFilesize: 2, // MB
-    autoProcessQueue: false,
-    uploadMultiple: false,
-    acceptedFiles: ".jpeg,.jpg,.png,.gif",
-    thumbnailWidth: null,
-    thumbnailHeight: null,
-    resizeQuality: 1,
-    resizeMethod: "crop",
-    init: function() {}
-});
+    let createNftUploadFile = new Dropzone(".createnftuploadfile", {
+        url: "/file/post",
+        paramName: "images", // The name that will be used to transfer the file
+        maxFiles: 1,
+        maxFilesize: 2, // MB
+        autoProcessQueue: false,
+        uploadMultiple: false,
+        acceptedFiles: ".jpeg,.jpg,.png,.gif",
+        thumbnailWidth: null,
+        thumbnailHeight: null,
+        resizeQuality: 1,
+        resizeMethod: "crop",
+        init: function() {}
+    });
 
-let createNftUploadDoc02 = new Dropzone(".createnftuploaddoc02", {
-    url: "/file/post",
-    paramName: "images", // The name that will be used to transfer the file
-    maxFiles: 1,
-    maxFilesize: 2, // MB
-    autoProcessQueue: false,
-    uploadMultiple: false,
-    acceptedFiles: ".jpeg,.jpg,.png,.gif",
-    thumbnailWidth: null,
-    thumbnailHeight: null,
-    resizeQuality: 1,
-    resizeMethod: "crop",
-    init: function() {}
-});
+    createNftUploadFile.on("addedfile", file => {
+        console.log(`File added: ${file.name}`);
+    });
+
+    let createNftUploadDoc01 = new Dropzone(".createnftuploaddoc01", {
+        url: "/file/post",
+        paramName: "images", // The name that will be used to transfer the file
+        maxFiles: 1,
+        maxFilesize: 2, // MB
+        autoProcessQueue: false,
+        uploadMultiple: false,
+        acceptedFiles: ".jpeg,.jpg,.png,.gif",
+        thumbnailWidth: null,
+        thumbnailHeight: null,
+        resizeQuality: 1,
+        resizeMethod: "crop",
+        init: function() {}
+    });
+
+    let createNftUploadDoc02 = new Dropzone(".createnftuploaddoc02", {
+        url: "/file/post",
+        paramName: "images", // The name that will be used to transfer the file
+        maxFiles: 1,
+        maxFilesize: 2, // MB
+        autoProcessQueue: false,
+        uploadMultiple: false,
+        acceptedFiles: ".jpeg,.jpg,.png,.gif",
+        thumbnailWidth: null,
+        thumbnailHeight: null,
+        resizeQuality: 1,
+        resizeMethod: "crop",
+        init: function() {}
+    });
+
+}
